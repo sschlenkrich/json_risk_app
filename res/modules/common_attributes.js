1fd33d5a8e40e31dbb972299fb972b0bf318d8ec
tilwolff <tilwolff@yahoo.com>
2023-12-21 22:18:40 +0100

    change common_attributes, no need to add id attribute since workers always do that anyway

--- res/modules/common_attributes.js
+++ res/modules/common_attributes.js
@@ -2,11 +2,10 @@
 exports.simulation_once=function(){
 // store (all string valued) instrument attributes into results object
 	var results=this.results;
-			for ([key,value] of Object.entries(this.instrument)){
-				if (key.charAt(0)==='$') continue;
-				if ('string' === typeof value) results[key]=value;
-			}
-	this.results.id=this.instrument.id;
+		for ([key,value] of Object.entries(this.instrument)){
+			if (key.charAt(0)==='$') continue;
+			if ('string' === typeof value) results[key]=value;
+		}
 	if(this.instrument.tenor) this.results.tenor=this.instrument.tenor + 'M';
 			
 }
